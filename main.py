073730274ac9c6a4c2df278098401db4cda73791
zherdev_denis <df.fpm@mail.ru>
2019-10-14 12:09:42 +0300

    Initiating User Sign-in

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from oic import rndstr
 from oic.oic import Client
 from oic.oic.message import ProviderConfigurationResponse
 from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+from oic.utils.http_util import Redirect
 
 #  get TenantID, ClientID and ClientSecret from Azure portal during app registration
 client = Client(client_authn_method=CLIENT_AUTHN_METHOD)
@@ -18,3 +20,31 @@ provider_info = ProviderConfigurationResponse(
 
 # Now the provider_info contains all necessary information about provider includes scopes
 client.provider_info = provider_info
+
+registration_response = client.register(provider_info["registration_endpoint"])
+
+# The response will also be stored in the client instance (registration_response attribute)
+# and some of the parameters will be unpacked and set as attributes on the client instance.
+
+def sign_in():
+    '''
+        This handler initiates the sign-in process by redirecting to the provider authorization endpoint
+    '''
+    session = {
+        "state": rndstr(),
+        "nonce": rndstr()
+    }
+
+    args = {
+        "client_id": client.client_id,
+        "response_type": "code",
+        "scope": ["openid"],
+        "nonce": session["nonce"],
+        "redirect_uri": client.registration_response["redirect_uris"][0],
+        "state": session["state"]
+    }
+
+    auth_req = client.construct_AuthorizationRequest(request_args=args)
+    login_url = auth_req.request(client.authorization_endpoint)
+
+    return Redirect(login_url)
