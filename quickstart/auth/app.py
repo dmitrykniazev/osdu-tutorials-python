9ae294bb30d13a91e70fd7fe392266f0232a07f7
Dmitry Kniazev <dmitry_kniazev@epam.com>
2019-10-17 14:27:22 -0500

    Fixing the prior merge

--- quickstart/auth/app.py
+++ quickstart/auth/app.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-# stub for python implementation
-import falcon
-
-
-class IndexResource:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        quote = {"its the index handler"}
-
-        resp.media = quote
-
-
-class AuthResource:
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        quote = {
-            'quote': (
-                "I've always been more interested in "
-                "the future than in the past."
-            ),
-            'author': 'Grace Hopper'
-        }
-
-        resp.media = quote
-
-
-api = falcon.API()
-api.add_route('/', IndexResource())
-api.add_route('/auth/callback', IndexResource())
-=======
 import falcon
 from oic.oic import AuthorizationResponse
 
@@ -79,4 +48,3 @@ class AuthResource:
         return self.client.do_access_token_request(state=aresp["state"],
                                                    request_args=args,
                                                    authn_method="client_secret_basic")
->>>>>>> 3c3e8e99149a37ee4187c1151e03c84cc3177f84
