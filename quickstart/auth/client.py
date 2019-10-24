629e2704ef494348e9085e2f8ca6f71553fd7eb4
Vadzim Tsandzer <ts.taiye@gmail.com>
2019-10-24 11:47:16 +0300

    added search endpoint

--- quickstart/auth/client.py
+++ quickstart/auth/client.py
@@ -1,36 +1,21 @@
-import os
 
 from oic.oic import Client
-from oic.oic.message import ProviderConfigurationResponse, RegistrationResponse
+from oic.oic.message import RegistrationResponse
 from oic.utils.authn.client import CLIENT_AUTHN_METHOD
+
+from settings import AUTH_BASE_URL, CLIENT_ID, CLIENT_SECRET
 
 redirect_url = 'http://localhost:8080/auth/callback'
 
 #  get TenantID, ClientID and ClientSecret from Azure portal during app registration
-meta_endpoint = os.environ.get('AUTH_META_ENDPOINT', 'https://<meta-endpoint>')
-authorization_endpoint = os.environ.get('AUTH_ENDPOINT', 'https://<authorization-server-url>')
-token_endpoint = os.environ.get('AUTH_TOKEN_ENDPOINT', 'https://<authorization-token-server-url>')
-client_id = os.environ.get('AUTH_CLIENT_ID', 'your_client_id')
-client_secret = os.environ.get('AUTH_CLIENT_SECRET', 'your_client_secret')
 
 client = Client(client_authn_method=CLIENT_AUTHN_METHOD, verify_ssl=False)
-client.provider_config(issuer=meta_endpoint)
-
-# It is also possible to discover provider info by provider URL using client.provider_config('providerURL')
-# Provider info can be configured accordingly (prefer):
-provider_info = ProviderConfigurationResponse(
-    authorization_endpoint=authorization_endpoint,
-    token_endpoint=token_endpoint,
-    scopes_supported=['openid', 'email', 'profile'],
-)
-
-# Now the provider_info contains all necessary information about provider includes scopes
-client.provider_info = provider_info
+client.provider_config(issuer=AUTH_BASE_URL)
 
 # Store the registration credits in clients
 registration_info = RegistrationResponse(
-    client_id=client_id,
-    client_secret=client_secret,
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
 )
 client.store_registration_info(registration_info)
 client.redirect_uris = [redirect_url]
--- quickstart/entry_api.py
+++ quickstart/entry_api.py
@@ -1,18 +1,13 @@
 import falcon
 
-from auth.app import LoginResource, AuthResource
-
-
-class PingResource:
-    def on_get(self, request: falcon.Request, response: falcon.Response) -> None:
-        response.media = {'result': 'pong'}
-
+from auth.api import LoginResource, AuthResource
+from search.api import SearchResource
 
 api = falcon.API()
-
-# Test endpoint
-api.add_route('/ping', PingResource())
 
 # Auth endpoints
 api.add_route('/', LoginResource())
 api.add_route('/auth/callback', AuthResource())
+
+# Search endpoint
+api.add_route('/search', SearchResource())
