1589e1db14d28a9dbd4f6c4a351603fde62295cd
Vadzim Tsandzer <ts.taiye@gmail.com>
2019-10-24 15:24:38 +0300

    added fetch api

--- quickstart/entry_api.py
+++ quickstart/entry_api.py
@@ -2,6 +2,7 @@ import falcon
 
 from auth.api import LoginResource, AuthResource
 from search.api import SearchResource
+from fetch.api import FetchResource
 
 api = falcon.API()
 
@@ -11,3 +12,6 @@ api.add_route('/auth/callback', AuthResource())
 
 # Search endpoint
 api.add_route('/search', SearchResource())
+
+# Fetch endpoint
+api.add_route('/fetch', FetchResource())
