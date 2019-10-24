17a4c4314b8d0a4ee13f27a0a4107ab67dd942e2
Vadzim Tsandzer <ts.taiye@gmail.com>
2019-10-24 15:46:43 +0300

    small refactoring of defaults env

--- quickstart/settings.py
+++ quickstart/settings.py
@@ -1,7 +1,6 @@
 import os
 
-AUTH_BASE_URL = os.environ.get('AUTH_BASE_URL', 'https://<base-endpoint>')
-CLIENT_ID = os.environ.get('AUTH_CLIENT_ID', 'your_client_id')
-CLIENT_SECRET = os.environ.get('AUTH_CLIENT_SECRET', 'your_client_secret')
-OSDU_API_BASE_URL = os.environ.get('OSDU_API_BASE_URL', 'osdu_base_endpoint')
-
+AUTH_BASE_URL = os.environ.get('AUTH_BASE_URL')
+CLIENT_ID = os.environ.get('AUTH_CLIENT_ID')
+CLIENT_SECRET = os.environ.get('AUTH_CLIENT_SECRET')
+OSDU_API_BASE_URL = os.environ.get('OSDU_API_BASE_URL')
